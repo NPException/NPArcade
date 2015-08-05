ee3ae2af682146400fd06409a40043f91e1e7279
NPException <npexception42@gmail.com>
2015-08-05 18:12:22 +0200

    made stool texture use less space

--- src/main/java/de/npe/mcmods/nparcade/client/render/models/ModelArcadeStool.java
+++ src/main/java/de/npe/mcmods/nparcade/client/render/models/ModelArcadeStool.java
@@ -1,5 +1,7 @@
 package de.npe.mcmods.nparcade.client.render.models;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import de.npe.mcmods.nparcade.common.lib.Reference;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
@@ -9,9 +11,10 @@ import net.minecraft.util.ResourceLocation;
 /**
  * Created by NPException (2015)
  */
+@SideOnly(Side.CLIENT)
 public class ModelArcadeStool extends ModelBase {
 	public static final ModelArcadeStool instance = new ModelArcadeStool();
-	public static final ResourceLocation texture = new ResourceLocation(Reference.MOD_IDENTIFIER + "textures/blocks/arcadeStool.png");
+	public static final ResourceLocation texture = new ResourceLocation(Reference.MOD_IDENTIFIER + "textures/blocks/nparcade_stool.png");
 
 	public ModelRenderer seat;
 	public ModelRenderer seatBottom;
@@ -25,8 +28,8 @@ public class ModelArcadeStool extends ModelBase {
 	public ModelRenderer legYellow;
 
 	public ModelArcadeStool() {
-		this.textureWidth = 64;
-		this.textureHeight = 64;
+		this.textureWidth = 32;
+		this.textureHeight = 38;
 		this.seatEdge2 = new ModelRenderer(this, 0, 30);
 		this.seatEdge2.setRotationPoint(0.0F, 0.0F, 4.0F);
 		this.seatEdge2.addBox(-3.0F, 0.0F, 0.0F, 6, 1, 1, 0.0F);
@@ -36,7 +39,7 @@ public class ModelArcadeStool extends ModelBase {
 		this.legGreen = new ModelRenderer(this, 15, 22);
 		this.legGreen.setRotationPoint(-2.0F, 2.0F, -2.0F);
 		this.legGreen.addBox(-1.0F, 0.0F, -1.0F, 2, 8, 2, 0.0F);
-		this.seatBottom = new ModelRenderer(this, 33, 0);
+		this.seatBottom = new ModelRenderer(this, 0, 33);
 		this.seatBottom.setRotationPoint(0.0F, 2.0F, 0.0F);
 		this.seatBottom.addBox(-2.0F, 0.0F, -2.0F, 4, 1, 4, 0.0F);
 		this.legRed = new ModelRenderer(this, 15, 11);
