0c90c029b6c1261ceae9b2211dbaa55362bf5cf4
NPException <npexception42@gmail.com>
2015-08-27 19:52:20 +0200

    Added spaaace to mod name

--- src/main/java/de/npe/mcmods/nparcade/common/lib/Reference.java
+++ src/main/java/de/npe/mcmods/nparcade/common/lib/Reference.java
@@ -6,7 +6,7 @@ public class Reference {
 	public static final String MOD_ID = "nparcade";
 	public static final String MOD_IDENTIFIER = MOD_ID + ":";
 	public static final String MOD_VERSION = "0.1";
-	public static final String MOD_NAME = "NPArcade";
+	public static final String MOD_NAME = "NP Arcade";
 
 	public static final String CLIENT_PROXY_CLASS = "de.npe.mcmods.nparcade.client.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "de.npe.mcmods.nparcade.common.CommonProxy";
