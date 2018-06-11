ba5604b9f63592bdf58054695bfcb70798fe4493
NPException <npexception42@gmail.com>
2018-06-11 22:36:21 +0200

    overly excited premature version bump to 1.0

--- src/main/java/de/npe/mcmods/nparcade/common/lib/Reference.java
+++ src/main/java/de/npe/mcmods/nparcade/common/lib/Reference.java
@@ -3,7 +3,7 @@ package de.npe.mcmods.nparcade.common.lib;
 public final class Reference {
 	public static final String MOD_ID = "nparcade";
 	public static final String MOD_IDENTIFIER = MOD_ID + ":";
-	public static final String MOD_VERSION = "0.1";
+	public static final String MOD_VERSION = "1.0";
 	public static final String MOD_NAME = "NP Arcade";
 
 	public static final String CLIENT_PROXY_CLASS = "de.npe.mcmods.nparcade.client.ClientProxy";
